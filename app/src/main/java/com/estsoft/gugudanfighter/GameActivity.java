82f549acc1a34883e9ece9aaa4261ce99e3d12b0
kickscar <kickscar@gmail.com>
2016-05-20 14:21:26 +0900

    UI 수정3

--- app/src/main/java/com/estsoft/gugudanfighter/GameActivity.java
+++ app/src/main/java/com/estsoft/gugudanfighter/GameActivity.java
@@ -29,11 +29,6 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
 
     private Timer timer = new Timer();
     private static final int TIME_LIMITS = 30; //seconds
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,10 +37,6 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
 
         // 게임 초기화
         initGame();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     @Override
@@ -147,42 +138,6 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
     // 랜덤 정수 만드는 메세드
     private int randomize(int from, int to) {
         return (int) (Math.random() * to) + from;
-    }
-
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    public Action getIndexApiAction() {
-        Thing object = new Thing.Builder()
-                .setName("Game Page") // TODO: Define a title for the content shown.
-                // TODO: Make sure this auto-generated URL is correct.
-                .setUrl(Uri.parse("http://host/path"))
-                .build();
-        return new Action.Builder(Action.TYPE_VIEW)
-                .setObject(object)
-                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
-                .build();
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
-        AppIndex.AppIndexApi.start(client, getIndexApiAction());
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        AppIndex.AppIndexApi.end(client, getIndexApiAction());
-        client.disconnect();
     }
 
     private class PlayGameTimerTask extends TimerTask {
